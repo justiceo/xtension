2165981ee7854b4f2253d255f42221ee691de802
Justice Ogbonna <justiceo@users.noreply.github.com>
2024-12-23 23:38:25 +0100

    Update manifest validator, make it blocking for build

--- tools/manifest-validator.js
+++ tools/manifest-validator.js
@@ -103,6 +103,30 @@ export class ManifestValidator {
     try {
       const allPaths = [
         ...(this.manifest.icons ? Object.values(this.manifest.icons) : []),
+        ...(this.manifest.content_scripts.js
+          ? [this.manifest.content_scripts.js]
+          : []),
+        ...(this.manifest.content_scripts.css
+          ? [this.manifest.content_scripts.css]
+          : []),
+        ...(this.manifest.action
+          ? Object.values(this.manifest.action.default_icon)
+          : []),
+        ...(this.manifest.action.default_popup
+          ? [this.manifest.action.default_popup]
+          : []),
+        ...(this.manifest.side_panel
+          ? [this.manifest.side_panel.default_path]
+          : []),
+        ...(this.manifest.options_page ? [this.manifest.options_page] : []),
+        ...(this.manifest.background
+          ? Object.values(this.manifest.background)
+          : []),
+        ...(this.manifest.web_accessible_resources
+          ? this.manifest.web_accessible_resources
+              .map((r) => r.resources)
+              .flat()
+          : []),
       ];
       allPaths.forEach((file) => {
         if (!fs.existsSync(path.join(this.basePath, file))) {
@@ -121,13 +145,13 @@ export class ManifestValidator {
         const localesPath = path.join(this.basePath, "_locales");
         if (!fs.existsSync(localesPath)) {
           throw new Error(
-            "Locales directory does not exist but default_locale is set.",
+            "Locales directory does not exist but default_locale is set."
           );
         }
         const localeDir = path.join(localesPath, this.manifest.default_locale);
         if (!fs.existsSync(localeDir)) {
           throw new Error(
-            `Locale directory ${localeDir} for default_locale does not exist.`,
+            `Locale directory ${localeDir} for default_locale does not exist.`
           );
         }
         return { message: "Locales validation", status: "PASS" };
@@ -137,16 +161,24 @@ export class ManifestValidator {
     } catch (error) {
       return { message: "Locales validation", status: "FAIL", error: error };
     }
+
+    // TODO: validaate that i18n keys are defined, properly in format __MSG_[key]__
   }
 
   validateVersion() {
     try {
-      if (/^0[0-9]+/.test(this.manifest.version)) {
-        throw new Error("Version number cannot have a preceding zero.");
+      if (/\b0\d+\b/.test(this.manifest.version)) {
+        throw new Error(
+          `Manifest version ${this.manifest.version} cannot have a preceding zero in any of its parts.`
+        );
       }
       return { message: "Version format validation", status: "PASS" };
     } catch (error) {
-      return { message: "Version format validation", status: "FAIL" };
+      return {
+        message: "Version format validation",
+        status: "FAIL",
+        error: error,
+      };
     }
   }
 
@@ -196,13 +228,51 @@ export class ManifestValidator {
 
   validateShortName() {
     try {
-      if (this.manifest.short_name && this.manifest.short_name.length > 12) {
+      const shortName = this.manifest.short_name;
+      if (!shortName) {
+        return { message: "Short name validation", status: "PASS" };
+      }
+
+      if (!shortName.startsWith("__MSG_") && shortName.length > 12) {
         throw new Error("Short name must be less than 12 characters.");
       }
-      return { message: "Short name validation", status: "PASS" };
+
+      if (shortName.startsWith("__MSG_")) {
+        const localeKey = shortName.replace("__MSG_", "").replace("__", "");
+        const localeMessage = this.getDefaultLocaleMessage(localeKey);
+        if (localeMessage.length > 12) {
+          throw new Error("Short name must be less than 12 characters.");
+        } else {
+          return { message: "Short name validation", status: "PASS" };
+        }
+      }
     } catch (error) {
       return { message: "Short name validation", status: "FAIL", error: error };
     }
+  }
+
+  getDefaultLocaleMessage(localeKey) {
+    if (!this.manifest.default_locale) {
+      throw new Error(`default_locale is not set.`);
+    }
+    const localeDir = path.join(
+      this.basePath,
+      "_locales",
+      this.manifest.default_locale
+    );
+    const localeFile = path.join(localeDir, "messages.json");
+    if (!fs.existsSync(localeFile)) {
+      throw new Error(
+        `Locale file ${localeFile} for default_locale does not exist.`
+      );
+    }
+    const localeData = JSON.parse(fs.readFileSync(localeFile, "utf8"));
+    if (!localeData[localeKey]) {
+      throw new Error(
+        `Locale key ${localeKey} does not exist in ${localeFile}.`
+      );
+    }
+    return localeData[localeKey].message;
   }
 
   runAllValidations() {
@@ -219,8 +289,17 @@ export class ManifestValidator {
       console.log(
         `${result.message.padEnd(30, ".")}${result.status.padStart(5)} ${
           result.error ? result.error : ""
-        }`,
+        }`
       );
     });
+
+    const hasFailedValidation = results.find(
+      (result) => result.status === "FAIL"
+    );
+    if (hasFailedValidation) {
+      throw new Error(
+        "Manifest validation failed: " + hasFailedValidation.message
+      );
+    }
   }
 }
